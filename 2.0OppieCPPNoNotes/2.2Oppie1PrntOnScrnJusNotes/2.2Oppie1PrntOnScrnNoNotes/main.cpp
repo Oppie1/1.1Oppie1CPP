46828bf2f7a7131014aaacf0eda587e0e9e97b2a
Oppie1 <atindall729@gmail.com>
2026-03-22 12:33:46 -0500

    Add files via upload

---
+++ 2.0OppieCPPNoNotes/2.2Oppie1PrntOnScrnJusNotes/2.2Oppie1PrntOnScrnNoNotes/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+
+//Demonstration another attribute of \n (new line character).
+//When you use the \n through sentence it puts text on new line.
+
+
+//Key point: Go ahead and work with the spacings and "\n" int the sentence.
+//e.g. Delete the space in front of "My" and they the sentences on the new lines will
+//no longer line up. Also, try deleting the "\n" character and see what happens.
