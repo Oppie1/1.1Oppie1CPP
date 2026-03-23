64ce180c7b0a115f5f110b2bf7c76e30b5e86622
Oppie1 <atindall729@gmail.com>
2026-03-23 16:36:33 -0500

    Add files via upload

---
+++ 2.0OppieCPPJusNotes/2.8Oppie1FunctionsJusNotes/1.8Oppie1FunctionsJusNotes/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+
+
+//This is a function prototype. It is necessary to place this at top of main as computers read from top down.  The definition/answer to this
+//function is below the main function main(){ main program  }  Definition here (just below main). Create a void prototype function that
+//prints something to the screen:
+//Code:
+
+
+int main() {
+
+	//This is a function call. It is just code that says execute the function and show result of whatever 
+	//is in the code block of the function (here just a cout/print to screen statement.
+	//Code:
+
+
+	return 0;
+}
+
+//This is the "definition" (answer) of the function. When computer see "printToScreenFunction()" in main it follows that 
+//instruction and shows the codeblock (what's inside the brackets like code below) -> {    do this computer (here cout statement below)}
+//Code:
+
+//void just means not to return anything directly to main() (or other functions, their "caller" but we'll talk about that later :). 
+// Main will "call" this function and the function will execute that function
+//Again, do whatever is inside curly braces {     }. Think of functions like mini programs that main 
+//can ask to perform an a
+
+//Key point: Prototype -> Call -> Definition
