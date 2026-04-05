455fa5d49de28540fd859e7b4a5bac98a0d3d354
Oppie1 <atindall729@gmail.com>
2026-04-05 13:04:17 -0500

    Add files via upload

---
+++ 2.0OppieCPPJusNotes/2.45.46Oppie1CompositionJusNts/2.46.47Oppie1CompositionJusNts/Birthday.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+//We create a Birthday class to outline what we need to do to gather the birthday information,
+//store that information in private storage and make a function to access that information.
+//Remember that only functions of the same class have access to private storage.
+
+//Declare a class and name it Birthday
+//CODE:
+
+
+//Declare a public access specifier
+//CODE:
+
+
+//Here is the constructor that gets fed Birthday data from main()s arguments.
+
+//Make constructor declaration/declare a constructor Birthday with three parameters int m, int d, int y (month, day, year)
+//CODE:
+	
+
+	//This function takes that information and prints it orderly to the screen. It is a "void"
+	//so that actually takes place from here. It runs then gives control back to main.
+//Declare a void function that prints the date (name it yourself -> Use your imagination ;)
+//CODE:
+	
+
+	//This is where the values/data from mains arguments are stored once initialized in the 
+	//Birthday(int m,int d, int y) function definition.
+
+//Declare a private access specifier.
+//CODE:
+
+
+//Declare 3 int member variables (use m_) to help track that they are member variables of the birthday class to represent month, day, year.
+//CODE:
+	
