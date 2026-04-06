fbb0ab82045a4c452c117dc9fd10dd07214001d4
Oppie1 <atindall729@gmail.com>
2026-04-06 14:55:23 -0500

    Add files via upload

---
+++ OppieTeatherJusNoteIsh/1.51Oppie1InheritanceTeather/1.51Oppie1Inheritance/Son.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Mother.h"
+#include "Son.h"
+using namespace std;
+
+
+//This the constructor that is automatially run. It creates that empty box/object we keep talking about ;)
+//The son has nothing it can run but it has access as it is a derived class to the base class mother's functions.
+Son::Son()
+{
+
+}
