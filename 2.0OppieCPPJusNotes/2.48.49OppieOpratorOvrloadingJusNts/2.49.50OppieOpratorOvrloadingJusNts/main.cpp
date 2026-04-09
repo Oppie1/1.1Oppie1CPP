798836119a65de7b20333b47c485678d0b017b13
Oppie1 <atindall729@gmail.com>
2026-04-08 22:09:25 -0500

    Add files via upload

---
+++ 2.0OppieCPPJusNotes/2.48.49OppieOpratorOvrloadingJusNts/2.49.50OppieOpratorOvrloadingJusNts/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "Claudia.h"
+using namespace std;
+
+//In this tutorial we cover operator overloading which is just a way to seed an object with a 
+//symbol, letter, number etc. that can be used through a program
+
+int main() {
+
+	//Instantiate two objects of the Claudia class a and b with the parameters 3 and 7 respectively.
+	//CODE:
+
+
+	//c is an object of the Claudia class with no parameters. The constructor set this object to 0
+
+	//Instantiate a third object c with no parameters.
+	//CODE:
+
+
+	//Here we call on the ^ operator which will then be overridden int he .cpp file to set this
+	//expression to add the two objects a and b.
+
+	//Assign the result of the expression to the object c.
+	//Use/write an expression using the overloaded ^ operator to add the two objects a and b.
+	//CODE:
+
+
+	//We see the answer out to the screen.
+	//Display/out put the value of c.num to the screen (object "." number from a and b addition)
+	//CODE:
+
+
+}
